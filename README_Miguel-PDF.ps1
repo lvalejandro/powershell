36b22d0ee417760f4439cfdc33ebdc3caa8e2b44
lvalejandro <115987884+lvalejandro@users.noreply.github.com>
2023-01-21 08:01:07 +0100

    Create README_Miguel-PDF.ps1

---
+++ README_Miguel-PDF.ps1
@@ -0,0 +1,5 @@
+algo rápido para poder sacar el texto de un pdf para un amigo
+en esencia se parsea todo el pdf en bruto u se guarda en un txt
+le servió para poder sacar después la información con un scrip suyo muy sencillo ya que buscaba una string muy particular que se repetia decenas de veces
+con la variación de la fecha
+se ha de tener acceso a la .dll 
