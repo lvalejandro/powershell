c49dc221fad5e2928f05ced101eadba8fdc8e73e
lvalejandro <115987884+lvalejandro@users.noreply.github.com>
2023-01-21 08:13:36 +0100

    Create README_KeykumoAD.ps1

---
+++ README_KeykumoAD.ps1
@@ -0,0 +1,4 @@
+mientras en mi periodo de practicas se realiza una tarea semanal
+esta tarea implica descargar el singon de cada usuario del dominio para tareas de auditoria
+esta tarea se hace mediante el explorador que muchas veces se quedaba colgado
+creo este script que permite exportar esos registros para después abrirlos directamente en un csv o excel 
