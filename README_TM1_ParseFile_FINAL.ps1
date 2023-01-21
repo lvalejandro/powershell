5cf7dcceec2595cc99e7e7ef54dcc235defae844
lvalejandro <115987884+lvalejandro@users.noreply.github.com>
2023-01-21 08:05:54 +0100

    Create README_TM1_ParseFile_FINAL.ps1

---
+++ README_TM1_ParseFile_FINAL.ps1
@@ -0,0 +1,4 @@
+en esencia se crea ya que tenemos decenas de TM1 sevidores en un entorno distribuido
+casa servidor usa dos puertos
+sacamos los detalles que queremos
+le damos formato de tal modo que se pueda exportar a csv y de ahí a una hoja de excel
