18a3d73e440857d4d6e09ca60e3b6509ff83e76b
lvalejandro <115987884+lvalejandro@users.noreply.github.com>
2023-01-21 08:09:09 +0100

    Create README_verbos_final.ps1

---
+++ README_verbos_final.ps1
@@ -0,0 +1,4 @@
+un simple forma de recordar cosas de PS
+se crean objetos o tablas hash, se trabaja con listas
+se hacen comprobaciones rutinarias
+en uno de los files: verbos_For y Foereach-Object.ps1  se muestra una solución alternativa a un for
